d916e5c787734457b893a1932b43ab921f56ac2d
Miles <miles.h.lin@gmail.com>
2019-06-23 17:54:37 -0700

    Event + EventQueue

--- statemachine.cpp
+++ statemachine.cpp
@@ -3,8 +3,26 @@
 namespace statemachine
 {
 
-Event::Event (char * name) : name(name) {
+Event::Event (char * name) : name(name) {}
+Event::Event () : name(nullptr) {}
+void Event::markProcessed () {
+    /*
+    Mark a event as 'processed' and mark it for deletion from the event queue.
+     */
+    this->processed = true;
+}
+char * Event::getName () {
+    return this->name;
+}
+
+EventQueue::EventQueue () {}
+void EventQueue::pushEvent (Event * event) {
+    for (int idx = 0; idx < 10; idx += 1) {
+        if (this->queue[idx].getName() == nullptr) {
+            this->queue[idx] = event;
+            break;
+        }
+    }
 }
 
 } // namespace statemachine
-
--- statemachine.h
+++ statemachine.h
@@ -8,6 +8,19 @@ namespace statemachine
         bool processed = false;
     public:
         Event(char * name);
-        void mark_processed();
+        Event();
+        void markProcessed();
+        char * getName();
+    };
+
+    class EventQueue
+    {
+        Event * queue [10];
+    public:
+        EventQueue();
+        void pushEvent(Event * event);
+        Event getEventWithName(char * name);
+        void discard();
+        int discardProcessedEvents();
     };
 } // namespace statemachine
